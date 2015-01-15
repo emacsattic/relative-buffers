16464ad32c233a12282553351d3231a33620c006
Malyshev Artem <proofit404@gmail.com>
2015-01-15 21:59:23 +0300

    Update copyright years.

--- relative-buffers.el
+++ relative-buffers.el
@@ -1,6 +1,6 @@
 ;;; relative-buffers.el --- Emacs buffers naming convention
 
-;; Copyright (C) 2014 by Malyshev Artem
+;; Copyright (C) 2014-2015 by Malyshev Artem
 
 ;; Author: Malyshev Artem <proofit404@gmail.com>
 ;; URL: https://github.com/proofit404/relative-buffers
